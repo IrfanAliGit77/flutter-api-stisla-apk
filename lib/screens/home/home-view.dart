9de97d81b8f0f3cf6bbeba6f53bb0ef516c8a858
IrfanAliGit77 <raygensh77@gmail.com>
2022-12-23 05:00:05 +0700

    Update Home View Page Do Logout

--- lib/screens/home/home-view.dart
+++ lib/screens/home/home-view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:apk_api/controllers/logout-cont.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,17 +9,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final logout = LogoutController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF4EEFF),
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: const Color.fromARGB(255, 26, 53, 255),
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          'Halaman Home',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: Column(
-        children: [
-          Text("Home Page"),
-        ],
-      ),
-    );
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Column(
+          children: [
+             MaterialButton(
+              minWidth: 500,
+              height: 50,
+              child: Text(
+                "Logout",
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              color: const Color.fromARGB(255, 26, 53, 255),
+              onPressed: () {
+                logout.postDataOut().then(
+                      (value) =>
+                          Navigator.of(context).popAndPushNamed('/login'),
+                    );
+              },
+            ),
+          ],
+        ),
+        ),
+      );
   }
 }
